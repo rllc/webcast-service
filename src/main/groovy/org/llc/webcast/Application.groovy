4ec42596a1f505f0ce3cb1dabf1180443d3b1600
Steven McAdams <smcadams86@gmail.com>
2016-03-20 10:00:02 -0500

    build application as a war

--- src/main/groovy/org/llc/webcast/Application.groovy
+++ src/main/groovy/org/llc/webcast/Application.groovy
@@ -2,14 +2,21 @@ package org.llc.webcast
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.builder.SpringApplicationBuilder
+import org.springframework.boot.context.web.SpringBootServletInitializer
 
 /**
  * Created by steve on 10/25/2015.
  */
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
     }
 }
